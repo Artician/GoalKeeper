3c71a6b5444224c7a9918744035c82acd06e78fb
Jacob Moses <mosesjf17@gmail.com>
2021-07-10 01:36:26 -0400

    1.) Complete Settings activity - Done for now 2.) Start work on IO service - working on it
    Settings activity updates
    
    Done:
    * Updated the settings layout.
    * Added code to handle all of the views
    
    To do:
    * Bind to AppIO
    * Bundle settings changes
    * Implement messenger code to get current settings and push changes
    
    MainActivity updates:
    
    Done:
    
    * Added code to bind to IO and get a test message
    
    To do:
    * Create debug textview with status of connection to IO
    * Link the buttons with starting a new app
    * Test messenger code
    
    AppIO Updates:
    
    Done:
    * Started code to handle messages from any service.
    * Started mapping out logic for request and reply codes to and from the service
    
    To do:
    * Create the Rooms database for user data and settings
    * add code to handle messages from MainActivity and Settings activities
    * Add code to check and update user settings to the database
    
    General To Do:
    Implement SharedPrefs for stability
    
    Signed-off-by: Jacob Moses <mosesjf17@gmail.com>

--- app/src/main/java/com/example/goalkeeper/AppSettings.java
+++ app/src/main/java/com/example/goalkeeper/AppSettings.java
@@ -1,43 +1,102 @@
-package com.example.goalkeeper;
+ package com.example.goalkeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class AppSettings extends AppCompatActivity {
+ public class AppSettings extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+        // Bundles to hold existing and new settings
 
-    // Overview:
-    // This activity's purpose is to give the user a place to customize the behavior of the app.
-    // It gains the current user settings from IO, and posts updated settings to IO to update the
-    // database.
+        Bundle existSettings, newSettings;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app_settings);
-    }
+        // View handlers
+        Spinner plannerSpinner, weekSpinner, notificationSpinner;
 
-    // Settings List:
-    //               [ <key>                | <default value>                ]
-    //               [ plannerModeDefault   | day                            ]
-    //                  * What activity the app starts the planner in
-    //                  * Values: day, week, month
-    //               [ weekModeDefault      | 7 day                          ]
-    //                  * What mode week activity loads in
-    //                  * Values: 3, 5, and 7 day
-    //               [ notificationUrgency  | normal                         ]
-    //                  * How agressive the notifications should be
-    //                  * Values:  Gentle - on screen notification
-    //                  *          Normal - on screen notification, vibration, wake from sleep
-    //                  *          Agressive - all above, plus interrupts do not disturb and sets
-    //                  *                      an alarm
-    //
+        // Arrays for spinner options
 
-    // To do:
-    //  1.) Get settings from IO
-    //  2.) onClick for submit button view bundles up current settings and sends them to IO if valid
-    //  3.) Create private boolean validation function to ensure all new values are valid selections
-    //  4.) onClick for Resync button sends command to reconnect to google calendar
-    //  5.) onClick for Disconnect button view sends disconnect command
+        String[] plannerOptions         = {"Day", "Week", "Month"};
+        String[] weekOptions            = {"3 day", "5 day", "7 day"};
+        String[] notificationOptions    = {"Gentle", "Normal", "Aggressive"};
+
+        // Array adapters
+
+        ArrayAdapter plannerAdapter, weekAdapter, notificationAdapter;
+
+        // Overview:
+        // This activity's purpose is to give the user a place to customize the behavior of the app.
+        // It gains the current user settings from IO, and posts updated settings to IO to update the
+        // database.
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_app_settings);
+
+            plannerSpinner      = (Spinner) findViewById(R.id.SA_PlannerSpinner);
+            weekSpinner         = (Spinner) findViewById(R.id.SA_WeekSpinner);
+            notificationSpinner = (Spinner) findViewById(R.id.SA_NotificationSpinner);
+
+            plannerSpinner.setOnItemSelectedListener(this);
+            weekSpinner.setOnItemSelectedListener(this);
+            notificationSpinner.setOnItemSelectedListener(this);
+
+            plannerAdapter      = new ArrayAdapter(this, android.R.layout.simple_spinner_item, plannerOptions);
+            weekAdapter         = new ArrayAdapter(this, android.R.layout.simple_spinner_item, weekOptions);
+            notificationAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, notificationOptions);
+
+            plannerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            weekAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            notificationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            plannerSpinner.setAdapter(plannerAdapter);
+            weekSpinner.setAdapter(weekAdapter);
+            notificationSpinner.setAdapter(notificationAdapter);
+
+            // Set default values for the spinners.
+            // recode this to take values from existSettings
+            plannerSpinner.setSelection(0);
+            weekSpinner.setSelection(2);
+            notificationSpinner.setSelection(1);
+        }
+
+         @Override
+         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+         }
+
+         @Override
+         public void onNothingSelected(AdapterView<?> parent) {
+
+         }
+
+         public void onClickSubmit(View view) {
+         }
+
+     // Settings List:
+        //               [ <key>                | <default value>                ]
+        //               [ plannerModeDefault   | day                            ]
+        //                  * What activity the app starts the planner in
+        //                  * Values: day, week, month
+        //               [ weekModeDefault      | 7 day                          ]
+        //                  * What mode week activity loads in
+        //                  * Values: 3, 5, and 7 day
+        //               [ notificationUrgency  | normal                         ]
+        //                  * How agressive the notifications should be
+        //                  * Values:  Gentle - on screen notification
+        //                  *          Normal - on screen notification, vibration, wake from sleep
+        //                  *          Aggressive - all above, plus interrupts do not disturb and sets
+        //                  *                      an alarm
+        //
+
+        // To do:
+        //  1.) Get settings from IO
+        //  2.) onClick for submit button view bundles up current settings and sends them to IO if valid
+        //  3.) Create private boolean validation function to ensure all new values are valid selections
+        //  4.) onClick for Resync button sends command to reconnect to google calendar
+        //  5.) onClick for Disconnect button view sends disconnect command
 
 }
--- app/src/main/java/com/example/goalkeeper/MainActivity.java
+++ app/src/main/java/com/example/goalkeeper/MainActivity.java
@@ -2,9 +2,25 @@ package com.example.goalkeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    MyIncomingHandler incomingHandler = new MyIncomingHandler();
+    boolean isBoundIO = false;
+    Messenger requestForIO;
+    Messenger replyFromIO = new Messenger(incomingHandler);
+
+    ServiceConnection connection = new MyServiceConnection();
 
     // Overview:
     // This activity is the point of entry for the app. When it is loaded, it initializes the app,
@@ -18,8 +34,70 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Start IO service
+        //Intent intentIO = new Intent(getApplicationContext(), AppIO.class);
+        //startService(intentIO);
     }
 
+    public void messageToIO(){
+        if(isBoundIO)
+        {
+            // Message structure:
+            // [<key>               |<value>                    ]
+            // [source              |calling activity           ]
+            // [request             |request code               ]
+            //
+            // Request codes:
+            // 100 - Request for acknowledgement
+            // 101 - Request to read data
+            // 102 - Request to write data
+            // 103 - Request for update to AppWebConnect
+            // 104 - Request to update notifications
+
+            Bundle data = new Bundle();
+            data.putString("source", "MainActivity");
+            data.putInt("request", 100);
+
+            Message message = Message.obtain();
+            message.replyTo = replyFromIO;
+            message.setData(data);
+            try {
+                requestForIO.send(message);
+            } catch (RemoteException e){
+                e.printStackTrace();
+            }
+        }
+        else{
+            Toast.makeText(this, "Service not bound", Toast.LENGTH_SHORT);
+        }
+    }
+
+    protected boolean bindIO(){
+        Intent intentIO = new Intent(getApplicationContext(), AppIO.class);
+        return bindService(intentIO, connection, Context.BIND_AUTO_CREATE);
+
+    }
+
+    class MyIncomingHandler extends Handler{
+        @Override
+        public void handleMessage(Message msg){
+            Bundle bundle = msg.getData();
+            // Do some stuff with the variables and the reply codes
+        }
+    }
+
+    class MyServiceConnection implements ServiceConnection {
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            requestForIO = new Messenger(service);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            requestForIO = null;
+        }
+    }
     // To do:
     //  1.) User Data function
     //  2.) Check IO Service state
